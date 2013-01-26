d54ef6589af617ebbf593b479151139ef7b0ef37
Andrew January <andrewjanuary@gmail.com>
2013-01-26 09:46:53 +0000

    Adds cache headers.

---
+++ cachesettings.rb
@@ -0,0 +1,13 @@
+class CacheSettings
+  def initialize app, settings
+    @app = app
+    @settings = settings
+  end
+
+  def call env
+    res = @app.call(env)
+    res[1]['Cache-Control'] = @settings[:cache_control] if @settings.has_key?(:cache_control)
+    res[1]['Expires'] = (Time.now + @settings[:expires]).utf.rfc2822 if @settings.has_key?(:expires)
+    res
+  end
+end
